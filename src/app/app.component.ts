1b01b2002b64419ea6fb31850b899e67c793312c
DevWedeloper <vicnathangabrielle@gmail.com>
2024-04-13 04:06:06 +0800

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  template: ` <router-outlet /> `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  title = 'angular-form-builder';
+}

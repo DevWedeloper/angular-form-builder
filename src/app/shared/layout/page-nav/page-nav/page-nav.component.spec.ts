5030588a31e5670b49be94d726a3fe32c91a975f
DevWedeloper <vicnathangabrielle@gmail.com>
2024-04-22 05:02:58 +0800

    feat: add page nav component

---
+++ src/app/shared/layout/page-nav/page-nav/page-nav.component.spec.ts
@@ -0,0 +1,92 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Component, DebugElement, viewChild } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { routes } from '../../../../app.routes';
+import { SectionIntroComponent } from '../../../section-intro/section-intro.component';
+import { SectionSubHeadingComponent } from '../../../section-sub-heading/section-sub-heading.component';
+import { PageNavLinkComponent } from '../page-nav-link/page-nav-link.component';
+import {
+  PageNavOutletComponent,
+  pageNavTemplate,
+} from '../page-nav-outlet/page-nav-outlet.component';
+import { PageNavComponent } from './page-nav.component';
+
+describe('PageNavComponent', () => {
+  @Component({
+    standalone: true,
+    imports: [
+      PageNavComponent,
+      PageNavOutletComponent,
+      SectionIntroComponent,
+      SectionSubHeadingComponent,
+    ],
+    template: `
+      <section appMainSection>
+        <app-section-intro name="Test" lead="Testable" />
+        <app-section-sub-heading id="h2-deaing">
+          H2 Heading
+        </app-section-sub-heading>
+        <h3 id="h3-heading">H3 Heading 1</h3>
+        <h3 id="test-heading">H3 Heading 2</h3>
+      </section>
+      <app-page-nav />
+      <app-page-nav-outlet />
+    `,
+  })
+  class TestHostComponent {
+    pageNav = viewChild.required<PageNavComponent>(PageNavComponent);
+    pageNavOutlet = viewChild.required<PageNavOutletComponent>(
+      PageNavOutletComponent,
+    );
+  }
+
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+  let debugEl: DebugElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PageNavComponent],
+      providers: [provideRouter(routes), { provide: pageNavTemplate }],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestHostComponent);
+    component = fixture.componentInstance;
+    debugEl = fixture.debugElement.query(By.directive(PageNavOutletComponent));
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component.pageNav).toBeTruthy();
+  });
+
+  it('should render content of page nav', () => {
+    const pageNav = debugEl.query(By.css('[data-testingId="page-nav"]'));
+    expect(pageNav).toBeTruthy();
+  });
+
+  it('should render correct amount of links', () => {
+    const getLinks = debugEl.queryAll(By.directive(PageNavLinkComponent));
+    expect(getLinks.length).toEqual(3);
+  });
+
+  it('should add class pl-4 when link is nested', () => {
+    const getLinks = debugEl.queryAll(By.directive(PageNavLinkComponent));
+    const targetLinkEl: HTMLElement = getLinks.find(
+      (link) => link.attributes['ng-reflect-fragment'] === 'h3-heading',
+    )?.nativeElement;
+    expect(targetLinkEl.classList).toContain('pl-4');
+  });
+
+  it('should set pageNavTemplate', () => {
+    expect(pageNavTemplate()).toBeTruthy();
+  });
+
+  it('should set pageNavTemplate to null on destroy', () => {
+    expect(pageNavTemplate()).toBeTruthy();
+    fixture.destroy();
+    expect(pageNavTemplate()).toBeFalsy();
+  });
+});
---
+++ src/app/shared/layout/page-nav/page-nav/page-nav.component.ts
@@ -0,0 +1,110 @@
+import { NgClass, isPlatformServer } from '@angular/common';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  OnDestroy,
+  PLATFORM_ID,
+  TemplateRef,
+  computed,
+  inject,
+  isDevMode,
+  signal,
+  viewChild,
+} from '@angular/core';
+import { HlmScrollAreaComponent } from '@spartan-ng/ui-scrollarea-helm';
+import { PageNavLinkComponent } from '../page-nav-link/page-nav-link.component';
+import { pageNavTemplate } from '../page-nav-outlet/page-nav-outlet.component';
+
+type SamePageAnchorLink = {
+  id: string;
+  label: string;
+  isNested: boolean;
+};
+
+@Component({
+  selector: 'app-page-nav',
+  standalone: true,
+  imports: [HlmScrollAreaComponent, NgClass, PageNavLinkComponent],
+  host: {
+    class: 'hidden xl:block text-sm',
+  },
+  template: `
+    <ng-template #pageNav>
+      <hlm-scroll-area data-testingId="page-nav" class="h-[calc(100vh-3.5rem)]">
+        <div class="space-y-2 px-1">
+          <h3 class="font-medium">On this page</h3>
+          <ul data-testingId="links" class="m-0 flex list-none flex-col">
+            @for (link of links(); track link.id) {
+              <app-page-nav-link
+                [ngClass]="{ 'pl-4': link.isNested }"
+                [fragment]="link.id"
+                [label]="link.label"
+              />
+            } @empty {
+              @if (isDevMode()) {
+                [DEV] Nothing to see here!
+              }
+            }
+          </ul>
+        </div>
+      </hlm-scroll-area>
+    </ng-template>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PageNavComponent implements AfterViewInit, OnDestroy {
+  private pageNavTpl = viewChild.required<TemplateRef<unknown>>('pageNav');
+
+  protected readonly isDevMode = signal(isDevMode());
+
+  private readonly platformId = inject(PLATFORM_ID);
+
+  /**
+   * Reference to the tag with the main content of the page.
+   * For this to work, the component should be added immediately after a tag with the [appMainSection] directive.
+   */
+  private page: HTMLElement = (inject(ElementRef).nativeElement as HTMLElement)
+    .previousSibling as HTMLElement;
+
+  protected links = computed(() => {
+    if (isPlatformServer(this.platformId)) {
+      if (isDevMode()) {
+        console.error(
+          'This component should not be used for non-SSG/SPA pages.',
+        );
+      }
+      return;
+    }
+
+    const selectors = [
+      '[appMainSection] app-section-sub-heading',
+      '[appMainSection] > h3',
+    ];
+    const headings = Array.from(
+      this.page.querySelectorAll(selectors.join(',')),
+    );
+    const links = headings.map((element) => {
+      const { id, children, localName, textContent } = element;
+      const isSubHeading = localName === 'app-section-sub-heading';
+      const label =
+        (isSubHeading ? children[0].childNodes[0].textContent : textContent) ??
+        '[DEV] Empty heading!';
+      if (isDevMode() && id === '') {
+        console.error(`[DEV] id missing for heading "${label}"`);
+      }
+      return { id, label, isNested: !isSubHeading };
+    });
+    return links as SamePageAnchorLink[];
+  });
+
+  ngAfterViewInit() {
+    if (!this.pageNavTpl()) return;
+    pageNavTemplate.set(this.pageNavTpl());
+  }
+
+  ngOnDestroy() {
+    pageNavTemplate.set(null);
+  }
+}

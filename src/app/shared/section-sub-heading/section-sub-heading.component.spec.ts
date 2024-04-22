6c9427fb7c3830d72840c4a2ab34fbad66ec2a42
DevWedeloper <vicnathangabrielle@gmail.com>
2024-04-23 00:10:35 +0800

    refactor: simplify test

--- src/app/shared/section-sub-heading/section-sub-heading.component.spec.ts
+++ src/app/shared/section-sub-heading/section-sub-heading.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { Component, DebugElement } from '@angular/core';
+import { Component, DebugElement, viewChild } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { SectionSubHeadingComponent } from './section-sub-heading.component';
 
@@ -8,17 +8,19 @@ describe('SectionSubHeadingComponent', () => {
   @Component({
     standalone: true,
     imports: [SectionSubHeadingComponent],
-    template: `<app-section-sub-heading data-testingId="test-host"
-      >Test</app-section-sub-heading
-    >`,
+    template: `<app-section-sub-heading>Test</app-section-sub-heading>`,
   })
-  class SectionSubHeadingTestHostComponent {}
+  class TestHostComponent {
+    sectionSubHeading = viewChild.required<SectionSubHeadingComponent>(
+      SectionSubHeadingComponent,
+    );
+  }
 
-  let component: SectionSubHeadingComponent;
   let fixture: ComponentFixture<SectionSubHeadingComponent>;
   let debugEl: DebugElement;
-  let subHeadingFixture: ComponentFixture<SectionSubHeadingTestHostComponent>;
-  let subHeadingDebugEl: DebugElement;
+  let hostComponent: TestHostComponent;
+  let hostFixture: ComponentFixture<TestHostComponent>;
+  let hostDebugEl: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -26,32 +28,29 @@ describe('SectionSubHeadingComponent', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(SectionSubHeadingComponent);
-    component = fixture.componentInstance;
     debugEl = fixture.debugElement;
-    subHeadingFixture = TestBed.createComponent(
-      SectionSubHeadingTestHostComponent,
-    );
-    subHeadingDebugEl = subHeadingFixture.debugElement.query(
+    hostFixture = TestBed.createComponent(TestHostComponent);
+    hostComponent = hostFixture.componentInstance;
+    hostDebugEl = hostFixture.debugElement.query(
       By.directive(SectionSubHeadingComponent),
     );
-    fixture.detectChanges();
+    hostFixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(hostComponent.sectionSubHeading).toBeTruthy();
   });
 
   it('should properly render content', () => {
-    const subHeadingEl: HTMLElement = subHeadingDebugEl.query(
-      By.css('[data-testingId="sub-heading"]'),
-    ).nativeElement;
+    const subHeadingEl: HTMLElement = hostDebugEl.nativeElement;
     expect(subHeadingEl.textContent?.trim()).toEqual('Test');
   });
 
   it('should add class -mt-12 to self if first is true', () => {
-    expect(debugEl.nativeElement.classList).not.toContain('-mt-12');
+    const subHeadingEl: HTMLElement = debugEl.nativeElement;
+    expect(subHeadingEl.classList).not.toContain('-mt-12');
     fixture.componentRef.setInput('first', true);
     fixture.detectChanges();
-    expect(debugEl.nativeElement.classList).toContain('-mt-12');
+    expect(subHeadingEl.classList).toContain('-mt-12');
   });
 });

605bf9968a3dcfa6292720a1093ed252a0610f29
DevWedeloper <vicnathangabrielle@gmail.com>
2024-04-22 23:37:23 +0800

    feat: add side nav component

---
+++ src/app/shared/layout/side-nav/side-nav/side-nav.component.spec.ts
@@ -0,0 +1,35 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { routes } from '../../../../app.routes';
+import { SideNavContentComponent } from '../side-nav-content.component';
+import { SideNavComponent } from './side-nav.component';
+
+describe('SideNavComponent', () => {
+  let component: SideNavComponent;
+  let fixture: ComponentFixture<SideNavComponent>;
+  let debugEl: DebugElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SideNavComponent],
+      providers: [provideRouter(routes)],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SideNavComponent);
+    component = fixture.componentInstance;
+    debugEl = fixture.debugElement;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render app-side-nav-content component', () => {
+    const sideNavContent = debugEl.query(By.directive(SideNavContentComponent));
+    expect(sideNavContent).toBeTruthy();
+  });
+});
---
+++ src/app/shared/layout/side-nav/side-nav/side-nav.component.ts
@@ -0,0 +1,20 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HlmScrollAreaComponent } from '@spartan-ng/ui-scrollarea-helm';
+import { SideNavContentComponent } from '../side-nav-content.component';
+
+@Component({
+  selector: 'app-side-nav',
+  standalone: true,
+  imports: [HlmScrollAreaComponent, SideNavContentComponent],
+  host: {
+    class:
+      'fixed top-12 z-30 -ml-2 hidden w-full shrink-0 px-2 pb-12 pt-6 text-sm md:sticky md:block',
+  },
+  template: `
+    <hlm-scroll-area visibility="hover" class="h-[calc(100vh-3.5rem)]">
+      <app-side-nav-content />
+    </hlm-scroll-area>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SideNavComponent {}

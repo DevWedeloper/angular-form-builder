28e0e10704be8595d8aa35a3798246d4fa86ee2b
DevWedeloper <vicnathangabrielle@gmail.com>
2024-04-23 17:54:14 +0800

    fix: add path to libs/ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
   content: [
     './src/**/*.{html,ts}',
-    './REPLACE_WITH_PATH_TO_YOUR_COMPONENTS_DIRECTORY/**/*.{html,ts}',
+    './libs/ui/**/*.{html,ts}',
   ],
   theme: {
     extend: {},

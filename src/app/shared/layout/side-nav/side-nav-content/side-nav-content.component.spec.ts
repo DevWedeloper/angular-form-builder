cd535f1df00f628db23a5991d12c85d3983ee019
DevWedeloper <vicnathangabrielle@gmail.com>
2024-04-23 12:52:05 +0800

    feat: add side nav content component

---
+++ src/app/shared/layout/side-nav/side-nav-content/side-nav-content.component.spec.ts
@@ -0,0 +1,56 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Component, DebugElement, viewChild } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { routes } from '../../../../app.routes';
+import { SideNavContentComponent } from './side-nav-content.component';
+
+describe('SideNavContentComponent', () => {
+  @Component({
+    standalone: true,
+    imports: [SideNavContentComponent],
+    template: `
+      <app-side-nav-content
+        (linkClicked)="linkClicked()"
+      ></app-side-nav-content>
+    `,
+  })
+  class TestHostComponent {
+    sideNavContent = viewChild.required<SideNavContentComponent>(
+      SideNavContentComponent,
+    );
+    linkClicked() {}
+  }
+
+  let component: TestHostComponent;
+  let fixture: ComponentFixture<TestHostComponent>;
+  let debugEl: DebugElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SideNavContentComponent],
+      providers: [provideRouter(routes)],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TestHostComponent);
+    component = fixture.componentInstance;
+    debugEl = fixture.debugElement.query(By.directive(SideNavContentComponent));
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component.sideNavContent).toBeTruthy();
+  });
+
+  it('linkClicked should emit when link is clicked', () => {
+    const linkClickedSpy = jest.spyOn(component, 'linkClicked');
+
+    const linkEl: HTMLElement = debugEl.query(
+      By.css('[data-testingId="link"]'),
+    ).nativeElement;
+    linkEl.click();
+
+    expect(linkClickedSpy).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ src/app/shared/layout/side-nav/side-nav-content/side-nav-content.component.ts
@@ -0,0 +1,37 @@
+import { ChangeDetectionStrategy, Component, output } from '@angular/core';
+import { HlmScrollAreaComponent } from '@spartan-ng/ui-scrollarea-helm';
+import { SideNavHeadingDirective } from '../side-nav-heading.directive';
+import { SideNavLinkDirective } from '../side-nav-link/side-nav-link.directive';
+import { SideNavLinksComponent } from '../side-nav-links/side-nav-links.component';
+
+@Component({
+  selector: 'app-side-nav-content',
+  standalone: true,
+  imports: [
+    HlmScrollAreaComponent,
+    SideNavLinkDirective,
+    SideNavLinksComponent,
+    SideNavHeadingDirective,
+  ],
+  host: {
+    class: 'block px-1',
+  },
+  template: `
+    <div class="pb-4">
+      <h4 spartanSideNavHeading>Heading</h4>
+      <app-side-nav-links>
+        <a
+          data-testingId="link"
+          (click)="linkClicked.emit()"
+          appSideNavLink="/.."
+        >
+          Link
+        </a>
+      </app-side-nav-links>
+    </div>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SideNavContentComponent {
+  linkClicked = output();
+}

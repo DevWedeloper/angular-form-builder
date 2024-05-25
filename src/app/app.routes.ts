64a5ae299a25cfc76954bd70427ec3ab89743a3a
DevWedeloper <vicnathangabrielle@gmail.com>
2024-05-25 18:12:49 +0800

    feat: add builder route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { BuilderComponent } from './pages/builder/builder.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'builder',
+    component: BuilderComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'builder',
+    pathMatch: 'full',
+  },
+];

753483b83e9cb8fdc82fe2a5c000f42cf1ae10fc
DevWedeloper <vicnathangabrielle@gmail.com>
2024-05-27 13:21:15 +0800

    feat: add withFetch

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes';
 
@@ -14,5 +15,6 @@ export const appConfig: ApplicationConfig = {
       }),
     ),
     provideClientHydration(),
+    provideHttpClient(withFetch()),
   ],
 };

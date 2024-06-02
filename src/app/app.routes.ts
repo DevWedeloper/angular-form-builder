ae7c86f77037c9eff2884dbf4e3583691da8c528
DevWedeloper <vicnathangabrielle@gmail.com>
2024-06-02 16:06:16 +0800

    feat: include guide component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,18 @@ export const routes: Routes = [
   {
     path: 'builder',
     loadComponent: () =>
-      import('./pages/builder/builder.component').then((m) => m.BuilderComponent),
+      import('./pages/builder/builder.component').then(
+        (m) => m.BuilderComponent,
+      ),
   },
   {
     path: '',
     redirectTo: 'builder',
     pathMatch: 'full',
+  },
+  {
+    path: 'guide',
+    loadComponent: () =>
+      import('./pages/guide/guide.component').then((m) => m.GuideComponent),
   },
 ];

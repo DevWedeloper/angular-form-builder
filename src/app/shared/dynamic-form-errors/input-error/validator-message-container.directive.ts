77478d10924fff4f9d6049a225709e4f674d327d
DevWedeloper <vicnathangabrielle@gmail.com>
2024-06-12 19:03:28 +0800

    refactor: uncapitalize first letter

--- src/app/shared/dynamic-form-errors/input-error/validator-message-container.directive.ts
+++ src/app/shared/dynamic-form-errors/input-error/validator-message-container.directive.ts
@@ -1,9 +1,9 @@
 import { Directive, inject, ViewContainerRef } from '@angular/core';
 
 @Directive({
-  selector: '[ValidatorMessageContainerDirective]',
+  selector: '[validatorMessageContainerDirective]',
   standalone: true,
-  exportAs: 'ValidatorMessageContainerDirective',
+  exportAs: 'validatorMessageContainerDirective',
 })
 export class ValidatorMessageContainerDirective {
   container = inject(ViewContainerRef);

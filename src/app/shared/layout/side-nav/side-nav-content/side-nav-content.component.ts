60687ae5b2d33d11a239d8e69fabc7c2d071858b
DevWedeloper <vicnathangabrielle@gmail.com>
2024-11-22 09:42:05 +0800

    refactor: remove unused import

--- src/app/shared/layout/side-nav/side-nav-content/side-nav-content.component.ts
+++ src/app/shared/layout/side-nav/side-nav-content/side-nav-content.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component, output } from '@angular/core';
-import { HlmScrollAreaComponent } from '@spartan-ng/ui-scrollarea-helm';
 import { SideNavHeadingDirective } from '../side-nav-heading.directive';
 import { SideNavLinkDirective } from '../side-nav-link/side-nav-link.directive';
 import { SideNavLinksComponent } from '../side-nav-links/side-nav-links.component';
@@ -8,7 +7,6 @@ import { SideNavLinksComponent } from '../side-nav-links/side-nav-links.componen
   selector: 'app-side-nav-content',
   standalone: true,
   imports: [
-    HlmScrollAreaComponent,
     SideNavLinkDirective,
     SideNavLinksComponent,
     SideNavHeadingDirective,
